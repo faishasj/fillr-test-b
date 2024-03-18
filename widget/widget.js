4b7d823abd8fb4c9df35d32c472f3da31d777b64
Faisha Surjatin <faisha.surjatin@monash.edu>
2024-03-19 02:02:40 +1100

    Create and send fields list from child frame to top

--- widget/widget.js
+++ widget/widget.js
@@ -1,24 +1,39 @@
-'use strict'
+"use strict";
 // Write your module here
 // It must send an event "frames:loaded" from the top frame containing a list of { name:label } pairs,
 // which describes all the fields in each frame.
 
 // This is a template to help you get started, feel free to make your own solution.
 function execute() {
-	try {
+  try {
     // Step 1 Scrape Fields and Create Fields list object.
+
+    // Map over all labels in frame to create fields list
+    var fields = Array.from(document.getElementsByTagName("label")).map(
+      (label) => {
+        // Get field element by quering id from label's "for" attribute
+        const field = document.getElementById(label.getAttribute("for"));
+        // Return object { name:label }
+        return { [field.name]: label.innerText };
+      }
+    );
+    console.log(fields);
+
     // Step 2 Add Listener for Top Frame to Receive Fields.
+
     if (isTopFrame()) {
-      window.addEventListener('message', (event) => {
+      window.addEventListener("message", (event) => {
+        console.log(event.data);
         // - Merge fields from frames.
         // - Process Fields and send event once all fields are collected.
       });
     } else if (!isTopFrame()) {
       // Child frames sends Fields up to Top Frame.
+      getTopFrame().postMessage(fields, "*");
     }
-	} catch (e) {
-		console.error(e)
-	}
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 execute();
@@ -31,5 +46,5 @@ function getTopFrame() {
 }
 
 function isTopFrame() {
-  return window.location.pathname == '/context.html';
+  return window.location.pathname == "/context.html";
 }
